5822dfac369314646f2a0a15e14464d0107ff067
Nate McMaster <nate.mcmaster@microsoft.com>
2016-06-15 10:40:45 -0700

    Setup and use a docker image of mssql on Travis

--- build.sh
+++ build.sh
@@ -43,4 +43,36 @@ if test ! -d $buildFolder; then
     fi
 fi
 
+# If you want to use Microsoft SQL Server in Docker, sign up for the private preview at http://sqlserveronlinux.com/
+if [ ! -z "$DOCKER_IMAGE" ];then
+    docker_ip="$(docker-machine ip 2>/dev/null || echo "127.0.0.1")"
+    if [ ! -z "$DOCKER_LOGIN" ]; then
+        docker login -e="$DOCKER_LOGIN" -u "$DOCKER_LOGIN" -p "$DOCKER_PASSWORD" "$DOCKER_REPO"
+    fi
+    container="$(docker run -d -e SA_PASSWORD=sa -e ACCEPT_EULA=y -p 1433:1433 "$DOCKER_IMAGE")"
+    echo "Docker container is $container"
+    export Test__SqlServer__DefaultConnection="Server=$docker_ip;User ID=sa;Password=sa;Connect Timeout=30"
+    if [ ! -z "$DOCKER_WAIT_FOR_DB" ]; then
+        max_wait=60 # two minutes
+        until nc -z $docker_ip 1433 ; do
+            echo "$(date) - waiting for db server ..."
+            sleep 2
+            let max_wait-=1
+            if [ $max_wait -lt 0 ]; then
+                echo "Max wait reached. Exiting 1"
+                docker rm --force "$container"
+                exit 1
+            fi
+        done
+    fi
+fi
+
 $buildFile -r $repoFolder "$@"
+exitcode=$?
+
+if [ ! -z "$container" ]; then
+    echo "Cleaning up container $container"
+    docker rm --force "$container"
+fi
+
+exit $exitcode
